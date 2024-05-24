a6e8483b36d512567fc1b5d3602277ca80e2d88f
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-24 16:45:47 +0200

    created folder snack 4

---
+++ snack 3 copy/js/script.js
@@ -0,0 +1,46 @@
+// SNACK 3
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+
+const bike = [
+    {
+        nome : 'S-Works Tarmac SL8', 
+        peso :'6.5'
+    },
+    {
+        nome : 'Aeroad CFR Di2', 
+        peso :'7.5'
+    },
+    {
+        nome : 'REACTO 8000', 
+        peso :'7.9'
+    },
+    {
+        nome : 'belador-8', 
+        peso :'7.7KG'
+    },
+    {
+        nome : 'TCR Advanced SL 0 DA', 
+        peso :'6.2'
+    },
+];
+
+
+// let lightBike = bike.filter((element) =>{
+//     if (element.peso < element.peso) {
+        
+//     }
+// });
+
+
+let lightBike = [];
+
+// console.log(lightBike);
+
+bike.forEach((element) => {
+    lightBike +=element.peso;
+});
+
+console.log(`${lightBike.nome}- ${lightBike.peso}`);
--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -1,46 +1,40 @@
-// SNACK 3
-// Creare un array di oggetti:
-// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// Stampare in console la bici con peso minore utilizzando destructuring e template literal
+// SNACK 4
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
 
-
-const bike = [
+const squad = [
     {
-        nome : 'S-Works Tarmac SL8', 
-        peso :'6.5'
+        nome : 'inter',
+        puntifatti :' 0',
+        fallisubiti : '0'
     },
     {
-        nome : 'Aeroad CFR Di2', 
-        peso :'7.5'
+        nome : 'milan',
+        puntifatti :' 0',
+        fallisubiti : '0'
     },
     {
-        nome : 'REACTO 8000', 
-        peso :'7.9'
+        nome : 'bologna',
+        puntifatti :' 0',
+        fallisubiti : '0'
     },
     {
-        nome : 'belador-8', 
-        peso :'7.7KG'
+        nome : 'lazio',
+        puntifatti :' 0',
+        fallisubiti : '0'
     },
     {
-        nome : 'TCR Advanced SL 0 DA', 
-        peso :'6.2'
+        nome : 'roma',
+        puntifatti :' 0',
+        fallisubiti : '0'
     },
 ];
 
+// let point = [];
 
-// let lightBike = bike.filter((element) =>{
-//     if (element.peso < element.peso) {
-        
-//     }
+// squad.forEach((element, index) => {
+//     let falli = 
 // });
 
 
-let lightBike = 0;
-
-console.log(lightBike);
-
-bike.forEach((element) => {
-    lightBike +=element.peso
-});
-
-console.log(` ${lightBike.nome}- ${lightBike.peso}`);
